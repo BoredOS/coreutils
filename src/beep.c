65887ab4a977c9d076b5f3496f89a92f1659836b
boreddevnl <chris@boreddev.nl>
2026-06-07 19:23:28 +0200

    update beep to use pcspk device

--- src/beep.c
+++ src/beep.c
@@ -2,12 +2,31 @@
 // This software is released under the GNU General Public License v3.0. See LICENSE file for details.
 // This header needs to maintain in any file it is present in, as per the GPL license terms.
 #include <stdlib.h>
-#include <syscall.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <dev/pcsk.h>
 
 int main(int argc, char **argv) {
     (void)argc; (void)argv;
-    sys_system(SYSTEM_CMD_BEEP, 392, 400, 0, 0);
+    int fd = open("/dev/pcsk", O_RDONLY);
+    if (fd < 0) {
+        perror("open /dev/pcsk");
+        return 1;
+    }
+
+    struct pcsk_beep b;
+    b.freq = 392;
+    b.ms = 400;
+
+    if (ioctl(fd, PCSK_IOCTL_BEEP, &b) < 0) {
+        perror("ioctl PCSK_IOCTL_BEEP");
+        close(fd);
+        return 1;
+    }
+
+    close(fd);
     printf("BEEP!\n");
     return 0;
 }
