b2a23025f733fc258e2acff2da525c04a4a068d7
boreddevnl <chris@boreddev.nl>
2026-08-21 15:07:20 +0200

    fix: kill command

--- src/kill.c
+++ src/kill.c
@@ -29,8 +29,8 @@ int main(int argc, char **argv) {
     }
 
     for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-9") == 0) {
-            sig = SIGKILL;
+        if (argv[i][0] == '-' && is_numeric(argv[i] + 1)) {
+            sig = atoi(argv[i] + 1);
         } else if (!pid_str) {
             pid_str = argv[i];
         } else {
